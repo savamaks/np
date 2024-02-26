4c0ae99dc259df16c02dea26ab9920c3cb988be3
Максим <105449753+savamaxxx@users.noreply.github.com>
2024-02-26 21:37:44 +0300

    изменил ширину загаловка карточки

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,24 +1,23 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React, { FC, ReactNode, MouseEvent } from "react";
-import s from './Button.module.scss'
+import s from "./Button.module.scss";
 
 interface IButton {
     children: ReactNode;
     path?: string;
     className?: string;
     onClick?: () => void;
-    
 }
 
 const Button: FC<IButton> = ({ children, className, path }) => {
     const router = useRouter();
 
-    const click = (e: MouseEvent<HTMLButtonElement>) => {
+    const clickCard = (e: MouseEvent<HTMLButtonElement>) => {
         router.push(`/services/${path}`);
     };
     return (
-        <button onClick={click} className={className}>
+        <button onClick={clickCard} className={className}>
             {children}
         </button>
     );
